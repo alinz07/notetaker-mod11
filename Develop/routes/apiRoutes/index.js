9b1d6f6fe74e064242edb079e11c6357c879246f
Tony Linz <tonyllinz@gmail.com>
2022-02-17 18:13:55 -0600

    successfully test routes for json and html

--- Develop/routes/apiRoutes/index.js
+++ Develop/routes/apiRoutes/index.js
@@ -1,14 +1,13 @@
 const router = require('express').Router();
-const { filterByQuery, findById, createNewAnimal, validateAnimals} = require('../../../public/assets/js/index.js');
-const { notes } = require('../../../db/db.json')
+//const { filterByQuery, findById, createNewAnimal, validateAnimals} = require('../../../public/assets/js/index.js');
+const notes = require('../../db/db.json');
 
-router.get('/api/notes', (req, res) => {
+router.get('/notes', (req, res) => {
     //read the db.json file and return all saved notes as json
-    
-    res.json(results);
+    res.json(notes);
 });
 
-router.post('/api/notes', (req,res) => {
+router.post('/notes', (req, res) => {
     //receive a new note to post on the request body
 
     //create a unique id for the note and look into npm packages that will do this
@@ -16,7 +15,7 @@ router.post('/api/notes', (req,res) => {
     //add it to the db.json file
 
     //return new note to the client
-    res.json(result);
+    res.json('Post Malone!');
     
 })
 
